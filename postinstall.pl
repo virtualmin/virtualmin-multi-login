ef3c3388c10959447091124c732896590d7438d5
Jamie Cameron <jcameron@webmin.com>
2008-04-11 23:43:45 +0000

    Use new anonymous API

--- postinstall.pl
+++ postinstall.pl
@@ -3,25 +3,7 @@ require 'virtualmin-multi-login-lib.pl';
 
 sub module_install
 {
-# Make this module available anonymously
-local %miniserv;
-&get_miniserv_config(\%miniserv);
-local @anon = split(/\s+/, $miniserv{'anonymous'});
-local $found = 0;
-foreach my $a (@anon) {
-	local ($path, $user) = split(/=/, $a);
-	$found++ if ($path eq "/$module_name");
-	}
-if (!$found) {
-	local %acl;
-	&read_acl(undef, \%acl);
-	local $defuser = $acl{'root'} ? 'root' :
-			 $acl{'admin'} ? 'admin' :
-			 (keys %acl)[0];
-	push(@anon, "/$module_name=$defuser");
-	$miniserv{'anonymous'} = join(" ", @anon);
-	&put_miniserv_config(\%miniserv);
-	&reload_miniserv();
-	}
+&foreign_require("acl", "acl-lib.pl");
+&acl::setup_anonymous_access("/$module_name", $module_name);
 }
 
